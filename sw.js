fecbaf1c0f61a29df2c55fab7775afab568640e6
Belacatalogo <106787549+Belacatalogo@users.noreply.github.com>
2026-04-25 19:22:09 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Fluency Service Worker — offline cache
-const CACHE = "fluency-v21";
+const CACHE = "fluency-v23";
 const STATIC = ["./", "./index.html", "./bundle.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
