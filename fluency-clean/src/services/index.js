302df5dcce7684e57ee6f01f5f76feb333d474fc
Belacatalogo <106787549+Belacatalogo@users.noreply.github.com>
2026-04-28 22:56:19 -0300

    Export auth and Firebase services

--- fluency-clean/src/services/index.js
+++ fluency-clean/src/services/index.js
@@ -3,3 +3,6 @@ export { diagnostics } from './diagnostics.js';
 export { LESSON_TYPES, normalizeLessonType, normalizeLesson, isReadingLesson } from './lessonTypes.js';
 export { GEMINI_LESSON_STATUS, generateLessonDraft, isValidGeminiKey, maskApiKey, normalizeLessonKeys } from './geminiLessons.js';
 export { AZURE_PRONUNCIATION_STATUS, analyzePronunciation } from './azurePronunciation.js';
+export { getFirebaseApp, getFirebaseAuth, getFirebaseStatus, hasFirebaseConfig } from './firebase.js';
+export { subscribeAuth, signInWithGoogle, logout } from './auth.js';
+export { getAccessSession, setAccessSession, clearAccessSession, validateAccessCode } from './accessCode.js';
