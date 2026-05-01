3f2ecf1fce26c336db8550ab8aafbcf02085edfc
Belacatalogo <106787549+Belacatalogo@users.noreply.github.com>
2026-05-01 13:25:20 -0300

    Practice rebuild 1: adiciona export central

---
+++ fluency-clean/src/practice/core/index.js
@@ -0,0 +1,6 @@
+export * from './PracticeTypes.js';
+export * from './PracticeNormalizer.js';
+export * from './PracticeQualityGate.js';
+export * from './PracticeAnswerChecker.js';
+export * from './PracticeSessionState.js';
+export * from './PracticeBuilder.js';
