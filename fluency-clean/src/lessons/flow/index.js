ac9607f8946d19687bb448efb10e682a6014642d
Belacatalogo <106787549+Belacatalogo@users.noreply.github.com>
2026-05-16 17:09:12 -0300

    feat: wire guided lesson flow exports

--- fluency-clean/src/lessons/flow/index.js
+++ fluency-clean/src/lessons/flow/index.js
@@ -1,3 +1,6 @@
+import './lesson-flow.css';
+import './lesson-phase.css';
+
 export { LessonFlowShell } from './LessonFlowShell.jsx';
 export { LessonFocusHeader } from './LessonFocusHeader.jsx';
 export { LessonPhaseStepper } from './LessonPhaseStepper.jsx';
@@ -13,3 +16,18 @@ export {
   isPhaseAttempted,
   safePhases,
 } from './lessonFlowProgress.js';
+
+export { PhaseShell } from './phases/PhaseShell.jsx';
+export { AttemptField } from './phases/AttemptField.jsx';
+export { ChoiceField } from './phases/ChoiceField.jsx';
+export { SpeakField } from './phases/SpeakField.jsx';
+export { ChecklistField } from './phases/ChecklistField.jsx';
+
+export { GrammarLessonFlow } from './grammar/GrammarLessonFlow.jsx';
+export { VocabularyLessonFlow } from './vocabulary/VocabularyLessonFlow.jsx';
+export { ReadingLessonFlowV2 } from './reading/ReadingLessonFlowV2.jsx';
+export { ListeningLessonFlow } from './listening/ListeningLessonFlow.jsx';
+export { SpeakingLessonFlow } from './speaking/SpeakingLessonFlow.jsx';
+export { WritingLessonFlow } from './writing/WritingLessonFlow.jsx';
+
+export * as textNormalize from './text/normalize.js';
